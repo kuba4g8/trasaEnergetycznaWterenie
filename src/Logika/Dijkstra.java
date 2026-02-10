4741fdf704965a4399c91ee200bd0984410d36e0
KaczoREX777 <adakutwa@gmail.com>
2026-02-10 20:32:01 +0100

    Poprawa algorytmu A* i Dijkstry

--- src/Logika/Dijkstra.java
+++ src/Logika/Dijkstra.java
@@ -9,26 +9,36 @@ public class Dijkstra
     public static List<Node> znajdzTrase(Grid grid, Node start, Node cel)
     {
         PriorityQueue<Node> openSet = new PriorityQueue<>();
+        Set<Node> openSetLookup = new HashSet<>();
+        Set<Node> closedSet = new HashSet<>();
         
         grid.resetGrid();
         
         start.gScore = 0;
         start.fScore = 0;
         openSet.add(start);
+        openSetLookup.add(start);
         
         while (!openSet.isEmpty())
         {
             Node current = openSet.poll();
+            openSetLookup.remove(current);
             
             if (current == cel)
             {
                 return reconstructPath(cel);
             }
             
+            closedSet.add(current);
+            
             List<Node> neigboursOfCurrent = grid.getNeighbors(current, grid.czyNaUkos);
             
             for (Node neighbor : neigboursOfCurrent)
             {
+                if (closedSet.contains(neighbor))
+                {
+                    continue;
+                }
                 
                 double kosztEnergii = grid.obliczEnergie(current, neighbor);
                 double finalnyGScore = current.gScore + kosztEnergii;
@@ -39,8 +49,14 @@ public class Dijkstra
                     neighbor.gScore = finalnyGScore;
                     neighbor.fScore = finalnyGScore;
                     
-                    if (!openSet.contains(neighbor))
+                    if (!openSetLookup.contains(neighbor))
                     {
+                        openSet.add(neighbor);
+                        openSetLookup.add(neighbor);
+                    }
+                    else
+                    {
+                        openSet.remove(neighbor);
                         openSet.add(neighbor);
                     }
                 }
--- src/Logika/aStar.java
+++ src/Logika/aStar.java
@@ -8,24 +8,35 @@ public class aStar {
     
     public static List<Node> znajdzTrase(Grid terrainMap, Node startPoint, Node targetPoint) {
         PriorityQueue<Node> nodesToExplore = new PriorityQueue<>();
+        Set<Node> openSet = new HashSet<>();
+        Set<Node> closedSet = new HashSet<>();
         
         terrainMap.resetGrid();
         
         startPoint.gScore = 0;
         startPoint.fScore = calculateHeuristic(startPoint, targetPoint);
         nodesToExplore.add(startPoint);
+        openSet.add(startPoint);
         
         while (!nodesToExplore.isEmpty())
         {
             Node current = nodesToExplore.poll();
+            openSet.remove(current);
             
             if (current == targetPoint)
             {
                 return buildFinalPath(targetPoint);
             }
             
+            closedSet.add(current);
+            
             for (Node neighbor : terrainMap.getNeighbors(current, terrainMap.czyNaUkos))
             {
+                if (closedSet.contains(neighbor))
+                {
+                    continue;
+                }
+                
                 double costToNeighbor = terrainMap.obliczEnergie(current, neighbor);
                 double tentativeGScore = current.gScore + costToNeighbor;
                 
@@ -33,11 +44,16 @@ public class aStar {
                 {
                     neighbor.parent = current;
                     neighbor.gScore = tentativeGScore;
-                    
                     neighbor.fScore = neighbor.gScore + calculateHeuristic(neighbor, targetPoint);
                     
-                    if (!nodesToExplore.contains(neighbor))
+                    if (!openSet.contains(neighbor))
                     {
+                        nodesToExplore.add(neighbor);
+                        openSet.add(neighbor);
+                    }
+                    else
+                    {
+                        nodesToExplore.remove(neighbor);
                         nodesToExplore.add(neighbor);
                     }
                 }
